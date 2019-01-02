5ee5acf8aa6323db42765efcbda7059a7830885f
Bram Enninf <bramenning@gmail.com>
2019-01-02 10:35:09 +0100

    eerste code voor extractie van data bij duo

--- src/duo/examenkandidaten.py
+++ src/duo/examenkandidaten.py
@@ -5,7 +5,7 @@ import numpy as np
 
 examenkandidaten_url = 'https://duo.nl/open_onderwijsdata/images/06-examenkandidaten-en-geslaagden-2013-2018.csv'
 
-def get_examenkandidaten(examenkandidaten_url):
+def get_examenkandidaten(examenkandidaten_url=examenkandidaten_url):
     data = pd.read_csv(examenkandidaten_url
                         , sep=";"
                         , encoding="latin1")
@@ -21,7 +21,7 @@ def select_columns_examenkandidaten():
 
     # lees data in:
     data = get_examenkandidaten()
-    target_cols = [col for col in df.keys().tolist() if ((('GESLAAGDEN' in col) or ('EXAMENKAND' in col) or ('-') not in col) and ('TOTAAL' not in col) and ('ONBEKEND' not in col))]
+    target_cols = [col for col in data.keys().tolist() if ((('GESLAAGDEN' in col) or ('EXAMENKAND' in col) or ('-') not in col) and ('TOTAAL' not in col) and ('ONBEKEND' not in col))]
     result = data[target_cols].copy()
     return result
 
@@ -90,47 +90,3 @@ def tidy_format_examenkandidaten():
     tidy = tidy[tidy.Examenkandidaten > 0].copy()
     
     return tidy
-
-
-def aggregeer(data, groupers = ['BRIN NUMMER', 'INSTELLINGSNAAM VESTIGING','GEMEENTENAAM','ONDERWIJSTYPE VO']):
-    '''Aggregeert en melt zodat een tabel ontstaat die gekoppeld kan worden met 1cho.
-    Het cohort is het cohort dat start bij een instelling aan het hoger onderwijs, dus 2013 wil zeggen dat scholieren hun 
-    diploma halen in het VO in 2012-2013 maar starten bij HSL in of na 2013-2014.
-    Belangrijke velden zijn: BRIN NUMMER, GEMEENTENAAM, ONDERWIJSTYP VO'''
-    
-    filter_vmbo = data['ONDERWIJSTYPE VO']!='VMBO'
-    
-    subset = data[filter_vmbo].copy()
-    
-    totaal_cols = [col for col in subset.keys().tolist() if 'GESLAAGDEN' in col and 'TOTAAL' in col]
-    result = subset.groupby(groupers).agg(sum)[totaal_cols]
-    result.columns = [col[-13:-9] for col in result.columns.tolist()]
-    result = result.reset_index()
-    result = pd.melt(result, id_vars=groupers, var_name='Cohort', value_name='Aantal')
-    result.rename(columns={'GEMEENTENAAM':'Gemeente','ONDERWIJSTYPE VO':'Vooropleiding', 'BRIN NUMMER': 'Brinnummer','INSTELLINGSNAAM VESTIGING':'NaamInstelling'}, inplace=True)
-    result.Cohort = result.Cohort.astype(int)
-    result = result[result.Cohort > 2014].copy()
-    
-    return result
-
-def get_duo_data_mbo():
-    url = mbo_url = 'https://duo.nl/open_onderwijsdata/images/10.-gediplomeerden-per-instelling%2C-plaats%2C-kenniscentrum%2C-sector%2C-sectorunit%2C-type-mbo%2C-opleiding%2C-niveau%2C-geslacht.csv'
-    data = pd.read_csv(mbo_url, sep=';', encoding='latin1')
-    return data
-
-def get_mbo_data():
-    mbo_ruw = get_duo_data_mbo()
-    
-    filter_niveau_4 = mbo_ruw.KWALIFICATIENIVEAU== 4
-    filter_bol = mbo_ruw['TYPE MBO'].isin(['BOLVT','BOLDT'])
-    mbo4 = mbo_ruw[(filter_niveau_4) & (filter_bol)].copy()
-    id_vars = ['BRIN NUMMER',
-                 'INSTELLINGSNAAM',
-                 'PLAATSNAAM',]
-    result = pd.melt(mbo4, id_vars=id_vars, var_name='Variabele', value_name='Aantal')
-    result = result[result.Variabele.str.contains('TOTAAL')].copy()
-    result['Cohort'] = result.Variabele.str[-4:]
-    del result['Variabele']
-    result.rename(columns={'PLAATSNAAM':'Plaatsnaam','INSTELLINGSNAAM':'NaamInstelling', 'BRIN NUMMER': 'Brinnummer'}, inplace=True)
-    result = result.groupby(['Brinnummer', 'NaamInstelling', 'Plaatsnaam','Cohort']).Aantal.sum().reset_index()
-    return result
---
+++ src/duo/gediplomeerden.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+import numpy as np
+
+gediplomeerden_url = 'https://duo.nl/open_onderwijsdata/images/10.-gediplomeerden-per-instelling%2C-plaats%2C-kenniscentrum%2C-sector%2C-sectorunit%2C-type-mbo%2C-opleiding%2C-niveau%2C-geslacht.csv'
+
+def get_gediplomeerden():
+    gediplomeerden_ur = gediplomeerden_url
+    data = pd.read_csv(gediplomeerden_ur, sep=';', encoding='latin1')
+    return data
+
+def get_mbo_data():
+    mbo_ruw = get_gediplomeerden()
+    
+    filter_niveau_4 = mbo_ruw.KWALIFICATIENIVEAU== 4
+    filter_bol = mbo_ruw['TYPE MBO'].isin(['BOLVT','BOLDT'])
+    mbo4 = mbo_ruw[(filter_niveau_4) & (filter_bol)].copy()
+    id_vars = ['BRIN NUMMER',
+                 'INSTELLINGSNAAM',
+                 'PLAATSNAAM',]
+    result = pd.melt(mbo4, id_vars=id_vars, var_name='Variabele', value_name='Aantal')
+    result = result[result.Variabele.str.contains('TOTAAL')].copy()
+    result['Cohort'] = result.Variabele.str[-4:]
+    del result['Variabele']
+    result.rename(columns={'PLAATSNAAM':'Plaatsnaam','INSTELLINGSNAAM':'NaamInstelling', 'BRIN NUMMER': 'Brinnummer'}, inplace=True)
+    result = result.groupby(['Brinnummer', 'NaamInstelling', 'Plaatsnaam','Cohort']).Aantal.sum().reset_index()
+    return result
