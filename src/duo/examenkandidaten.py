a008f931e61b555942d9106d562423957f17ad1e
Bram Enninf <bramenning@gmail.com>
2019-01-02 20:21:44 +0100

    python linting

--- src/duo/examenkandidaten.py
+++ src/duo/examenkandidaten.py
@@ -1,79 +1,73 @@
-import os
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 
 examenkandidaten_url = 'https://duo.nl/open_onderwijsdata/images/06-examenkandidaten-en-geslaagden-2013-2018.csv'
 
-def get_examenkandidaten(examenkandidaten_url=examenkandidaten_url):
-    data = pd.read_csv(examenkandidaten_url
-                        , sep=";"
-                        , encoding="latin1")
-    return data
 
+def get_examenkandidaten(examenkandidaten_url=examenkandidaten_url):
+    """Download csv bestand van DUO"""
+    data = pd.read_csv(examenkandidaten_url, sep=";", encoding="latin1")
+    return data
 
 
 def select_columns_examenkandidaten():
     """Selecteer alleen die kolommen die we nodig hebben. Hier worden slaagpercentages
-    verwijderd, dat is een tenslotte het resultaat van delen van geslaagden door examenkandidaten en worden 
+    verwijderd, dat is een tenslotte het resultaat van delen van geslaagden door examenkandidaten en worden
     de TOTAAL-kolommen verwijderd. Dat is tenslotte het resultaat van een sommering van de gegevens.
     Ook de ONBEKENDEN worden verwijderd, die zijn er niet of nauwelijks en leiden alleen maar af."""
 
     # lees data in:
     data = get_examenkandidaten()
-    target_cols = [col for col in data.keys().tolist() if ((('GESLAAGDEN' in col) or ('EXAMENKAND' in col) or ('-') not in col) and ('TOTAAL' not in col) and ('ONBEKEND' not in col))]
+    target_cols = [col for col in data.keys().tolist() if ((('GESLAAGDEN' in col) or ('EXAMENKAND' in col) or ('-')
+                                                            not in col) and ('TOTAAL' not in col) and ('ONBEKEND' not in col))]
     result = data[target_cols].copy()
     return result
+
 
 def _examenkandidaten_en_geslaagden_op_een_regel(data):
     """Zet Geslaagden en Examenkandidaten op een regel."""
     fields = ['BRIN NUMMER',
-             'VESTIGINGSNUMMER',
-             'INSTELLINGSNAAM VESTIGING',
-             'GEMEENTENAAM',
-             'ONDERWIJSTYPE VO',
-             'INSPECTIECODE',
-             'OPLEIDINGSNAAM',
-             'Geslacht',
-             'Cohort']
+              'VESTIGINGSNUMMER',
+              'INSTELLINGSNAAM VESTIGING',
+              'GEMEENTENAAM',
+              'ONDERWIJSTYPE VO',
+              'INSPECTIECODE',
+              'OPLEIDINGSNAAM',
+              'Geslacht',
+              'Cohort']
 
     # maak een set met geslaagden
     geslaagden = data[data.Geslaagden.notnull()].copy()
     del geslaagden['Examenkandidaten']
-    
+
     # maak een set met examenkandidaten
     ex_kandidaten = data[data.Examenkandidaten.notnull()].copy()
     del ex_kandidaten['Geslaagden']
-    
+
     # voeg ze samen
-    result = pd.merge(ex_kandidaten
-            , geslaagden
-            , left_on=fields
-            , right_on=fields
-            , how='outer')
-    
+    result = pd.merge(ex_kandidaten, geslaagden, left_on=fields, right_on=fields, how='outer')
+
     return result
 
 
 def tidy_format_examenkandidaten():
-    """Zet de tabel om naar tidy-format zonder informatieverlies. Er komen aparte kolommen voor 
+    """Zet de tabel om naar tidy-format zonder informatieverlies. Er komen aparte kolommen voor
     Geslaagden en Examenkandidaten.
     """
     data = select_columns_examenkandidaten()
-    result = pd.melt(data
-           , id_vars =['BRIN NUMMER', 'VESTIGINGSNUMMER', 'INSTELLINGSNAAM VESTIGING',
-                       'GEMEENTENAAM', 'ONDERWIJSTYPE VO', 'INSPECTIECODE', 'OPLEIDINGSNAAM']
-           ,value_name='Aantal')
-        
-    result['Geslacht'] = np.where(result.variable.str.contains('MAN'), 'Man' , np.nan)
-    result['Geslacht'] = np.where(result.variable.str.contains('VROUW'), 'Vrouw' , result['Geslacht'])
+    result = pd.melt(data, id_vars=['BRIN NUMMER', 'VESTIGINGSNUMMER', 'INSTELLINGSNAAM VESTIGING',
+                                    'GEMEENTENAAM', 'ONDERWIJSTYPE VO', 'INSPECTIECODE', 'OPLEIDINGSNAAM'], value_name='Aantal')
+
+    result['Geslacht'] = np.where(result.variable.str.contains('MAN'), 'Man', np.nan)
+    result['Geslacht'] = np.where(result.variable.str.contains('VROUW'), 'Vrouw', result['Geslacht'])
     assert result.Geslacht.isnull().sum() == 0
-    
-    result['Examenkandidaten'] = np.where(result.variable.str.contains('EXAMENKANDIDATEN '), result.Aantal , np.nan)
+
+    result['Examenkandidaten'] = np.where(result.variable.str.contains('EXAMENKANDIDATEN '), result.Aantal, np.nan)
     result['Examenkandidaten'] = pd.to_numeric(result.Examenkandidaten)
-    result['Geslaagden'] = np.where(result.variable.str.contains('GESLAAGDEN '), result.Aantal , np.nan)
+    result['Geslaagden'] = np.where(result.variable.str.contains('GESLAAGDEN '), result.Aantal, np.nan)
     result['Geslaagden'] = pd.to_numeric(result.Geslaagden)
-    
+
     # maak een tussentabel aan waarbij het eerste jaar wordt afgesplitst:
     jaren = result.variable.str.split('-', expand=True)
     jaren['Cohort'] = jaren[0].str[-4:]
@@ -86,7 +80,7 @@ def tidy_format_examenkandidaten():
     del result['Aantal']
 
     tidy = _examenkandidaten_en_geslaagden_op_een_regel(result)
-    
+
     tidy = tidy[tidy.Examenkandidaten > 0].copy()
-    
+
     return tidy
